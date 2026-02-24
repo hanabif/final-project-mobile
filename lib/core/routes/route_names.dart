ba47c116c2cc99903dbc55fb0b082023ac2dd424
haymi <haymanotaweke9@gmail.com>
2026-02-24 23:46:43 +0300

    fixed routing

--- lib/core/routes/route_names.dart
+++ lib/core/routes/route_names.dart
@@ -2,4 +2,7 @@ class RouteNames {
   static const String login = '/';
   static const String register = '/register';
   static const String home = '/home';
+  static const String forgotPassword = '/forgot-password';
+  static const String verifyCode = '/verify-code';
+  static const String resetPassword = '/reset-password';
 }
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,7 +5,8 @@ import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
 import 'features/auth/domain/usecases/login_usecase.dart';
 import 'features/auth/domain/usecases/register_usecase.dart';
-import 'features/auth/presentation/cubit/auth_cubit.dart';
+import 'features/auth/presentation/cubit/auth/auth_cubit.dart';
+
 
 final sl = GetIt.instance;
 
