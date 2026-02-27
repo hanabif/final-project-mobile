84a65f9363f77b80efbe2f2e785979313e9b824e
haymi <haymanotaweke9@gmail.com>
2026-02-27 13:19:36 +0300

    refactor: update dependency injection for password reset and verification use cases

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,4 @@
+import 'package:complaint_resolution_app/features/auth/presentation/cubit/password_reset/password_reset_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'core/network/api_client.dart';
 import 'features/auth/data/datasources/auth_remote_datasource.dart';
@@ -6,7 +7,9 @@ import 'features/auth/domain/repositories/auth_repository.dart';
 import 'features/auth/domain/usecases/login_usecase.dart';
 import 'features/auth/domain/usecases/register_usecase.dart';
 import 'features/auth/presentation/cubit/auth/auth_cubit.dart';
-
+import 'features/auth/domain/usecases/reset_password_usecase.dart';
+import 'features/auth/domain/usecases/verify_code_usecase.dart';
+import 'features/auth/domain/usecases/forgot_password_usecase.dart';
 
 final sl = GetIt.instance;
 
@@ -30,5 +33,14 @@ Future<void> init() async {
   sl.registerLazySingleton(() => ForgotPasswordUseCase(sl()));
 
   // Auth - Presentation
-  sl.registerFactory(() => AuthCubit(loginUseCase: sl(), registerUseCase: sl(), resetPasswordUseCase: sl(), verifyCodeUseCase: sl(), forgotPasswordUseCase: sl()));
+  sl.registerFactory(
+    () => AuthCubit(loginUseCase: sl(), registerUseCase: sl()),
+  );
+  sl.registerFactory(
+    () => PasswordResetCubit(
+      forgotPasswordUseCase: sl(),
+      verifyCodeUseCase: sl(),
+      resetPasswordUseCase: sl(),
+    ),
+  );
 }
