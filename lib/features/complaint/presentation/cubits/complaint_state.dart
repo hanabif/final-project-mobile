afbb4d16c2e30ebad6013991ecec8c62e7735cfd
haymi <haymanotaweke9@gmail.com>
2026-03-09 12:52:31 +0300

    implement complaint cubit with submission states

---
+++ lib/features/complaint/presentation/cubits/complaint_state.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+abstract class ComplaintState extends Equatable {
+  const ComplaintState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ComplaintInitial extends ComplaintState {}
+
+class ComplaintSubmitting extends ComplaintState {}
+
+class ComplaintSuccess extends ComplaintState {}
+
+class ComplaintFailure extends ComplaintState {
+  final String message;
+
+  const ComplaintFailure({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
