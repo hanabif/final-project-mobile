1f5c25a989a8fa417f594af65f4b8cfcb42252c7
haymi <haymanotaweke9@gmail.com>
2026-05-11 18:40:44 +0300

    fix the session validation

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../features/auth/presentation/pages/splash_page.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/register_page.dart';
 import '../../features/complaint/domain/entities/complaint.dart';
@@ -17,6 +18,11 @@ import 'route_names.dart';
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case RouteNames.splash:
+        return MaterialPageRoute(
+          builder: (_) => const SplashPage(),
+        );
+
       case RouteNames.login:
         return MaterialPageRoute(
           builder: (_) => const LoginPage(),
